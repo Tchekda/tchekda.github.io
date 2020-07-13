b0d6ba3640c5426d8021e2548d7e0434d9b32e90
GitHub Action <action@github.com>
2020-07-13 09:35:08 +0000

    Updates compiled CSS files

---
+++ tmp/config.rb
@@ -0,0 +1,17 @@
+require 'compass/import-once/activate'
+
+http_path = "/"
+
+if environment == :production
+   css_dir = "assets/css"  
+   output_style = :compressed
+   sourcemap = true
+else
+   output_style = :expanded
+   css_dir = "assets/css"  
+end
+sass_dir = "assets/_scss"
+
+
+relative_assets = true
+line_comments = false
