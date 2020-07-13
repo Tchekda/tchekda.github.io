acb4dff82f3f03514437cd0b2a5c02ac5cb8ba08
Tchekda (ASUS) <contact@tchekda.fr>
2020-07-13 23:02:35 +0200

    Fixinx sourcemap

--- config.rb
+++ config.rb
@@ -9,6 +9,6 @@ images_dir = "assets/img"
 output_style = (environment == :production) ? :compressed : :expanded
 
 
-relative_assets = true
+relative_assets = false
 line_comments = false
 sourcemap = true
